45797975b751d8904978d45d86275c920b060c0f
Vesdii <andrew.vesdii@gmail.com>
2019-03-24 21:34:07 -0700

    idk what changed but it's better

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,20 +1,19 @@
-﻿using System;
+﻿using System.Diagnostics;
 using System.Windows;
-using System.Diagnostics;
 
 namespace ezShutdown
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
             HourTextBox.Focus();
         }
-        
+
         private void HighlightText(object sender, RoutedEventArgs e)
         {
             if (HourTextBox.IsFocused) HourTextBox.SelectAll();
@@ -23,27 +22,24 @@ namespace ezShutdown
 
         private void ScheduleButton_Click(object sender, RoutedEventArgs e)
         {
-            if (!Byte.TryParse(HourTextBox.Text, out byte h)) h = 0;
-            if (!Byte.TryParse(MinTextBox.Text, out byte m)) m = 0;
-            
+            if (!byte.TryParse(HourTextBox.Text, out byte h)) h = 0;
+            if (!byte.TryParse(MinTextBox.Text, out byte m)) m = 0;
+
             int time = (h * 3600) + (m * 60);
             if (time == 0) return;
-
-            char type_Char = 's';
-            string type_String = "Shutdown";
+			
+            string option = "Shutdown";
             switch (TypeComboBox.SelectedIndex)
             {
                 case 0:
-                    type_Char = 's';
-                    type_String = "Shutdown";
+                    option = "Shutdown";
                     break;
                 case 1:
-                    type_Char = 'r';
-                    type_String = "Restart";
+                    option = "Restart";
                     break;
             }
 
-            string confirmPrompt = type_String + " in:  ";
+            string confirmPrompt = option + " in:  ";
             if (h != 0) confirmPrompt += h + "h ";
             if (m != 0) confirmPrompt += m + "m";
 
@@ -51,17 +47,20 @@ namespace ezShutdown
                 confirmPrompt, "Confirm", MessageBoxButton.OKCancel);
             if (result == MessageBoxResult.Cancel) return;
 
-            Process.Start("shutdown.exe", "-" + type_Char + " -t " + time);
+            Process.Start("shutdown.exe", "-" + option.ToLower()[0] + " -t " + time);
+
+            Close();
         }
 
         private void AbortButton_Click(object sender, RoutedEventArgs e)
         {
             Process.Start("shutdown.exe", "-a");
-        }
+			HourTextBox.Focus();
+		}
 
         private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
-            if (e.Key == System.Windows.Input.Key.Escape) Close();
+            if (e.Key == System.Windows.Input.Key.Subtract) Close();
         }
     }
 }
